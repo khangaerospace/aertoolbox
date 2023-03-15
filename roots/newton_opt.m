94e2a7f8052d7b18d739932db92a0c4e18799f37
Khang Nguyen <62628865+khangaerospace@users.noreply.github.com>
2023-03-15 19:29:36 -0400

    Create newton_opt.m

---
+++ roots/newton_opt.m
@@ -0,0 +1,19 @@
+function [x,fmin,path] = newton_opt(fun,x0,tol)
+    xi = x0;
+    [f,df,ddf] = fun(xi);
+    path = xi;
+    count = [1:6];
+    nor = zeros(0,6);
+    i = 1;
+    while norm(ddf\df) > tol
+        [f,df,ddf] = fun(xi);
+        xi = xi - (ddf\df);
+        nor(i) = norm(ddf\df);
+        path = [path,xi];
+        i = i+1;
+    end
+    plot(count,nor)
+    x = xi;
+    fmin = f;
+    
+end
